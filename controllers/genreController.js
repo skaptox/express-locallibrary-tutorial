2a44ecee23275b8d05440fa4896c822eecc9f224
Oscar Albornoz <oscar@ignisgravitas.com>
2018-12-23 12:54:31 -0400

    'Part 5: Displaying library data' completed

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,13 +1,52 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+const async = require('async');
 const Genre = require('../models/genre');
+const Book = require('../models/book');
 
 // Display list of all Genre.
 exports.genre_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre list');
+  Genre.find()
+    .sort([['name', 'ascending']])
+    .exec((err, list_genre) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      res.render('genre_list', { title: 'Genre List', genre_list: list_genre });
+    });
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function (req, res) {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+exports.genre_detail = function (req, res, next) {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        var err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render('genre_detail', {
+        title: 'Genre Detail',
+        genre: results.genre,
+        genre_books: results.genre_books
+      });
+    }
+  );
 };
 
 // Display Genre create form on GET.
